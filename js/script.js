6b9eedb17207fb7e2064a5157995e4f3a8c30b5a
Yaroslav Serhiienko <yaroslav.serhiienko@gmail.com>
2016-11-25 11:29:28 +0200

    Изменения 1

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 var link = document.querySelector(".index-info-button");
 var popup = document.querySelector(".modal-content");
 var close = document.querySelector(".modal-content-close");
+var login = popup.querySelector("[name=your-name]");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("modal-content-show");
+  login.focus();
 });
 
 close.addEventListener("click", function(event) {
